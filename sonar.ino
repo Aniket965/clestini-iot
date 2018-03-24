1f8197c541c9c6743c101e8815f330a73ff7f7e8
aniket sharma <aniket965.as@gmail.com>
2018-03-24 23:45:34 +0530

    Added Angle Support

--- sonar.ino
+++ sonar.ino
@@ -1,25 +1,52 @@
+#include <Servo.h>
+
 int triger_pin = 2;
 int echo_pin = 4;
+long prev_duration = 0;
+long current_duration = 0;
+float distance_cm = 0; 
+int angle = 0;
+
+Servo servo;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  servo.attach(9);
 }
 
 void loop() {
+  delay(1000);
+
+   for(angle= 90; angle >= 1; angle -= 1)     
+  {                                  
+    servo.write(angle);
+    current_duration = get_duration();
+    if(prev_duration < current_duration)) {
+       Serial.print(angle);
+       Serial.print(" ,");
+       distance_cm = current_duration/29/2;
+       Serial.print(distance_cm);
+       Serial.println();
+      }
+    get_duration();
+    delay(100);                       
+  } 
+  delay(1000);
+}
+
+long get_duration() {
   long duration;
-  pinMode(triger_pin,OUTPUT);
-  
+  pinMode(triger_pin,OUTPUT);  
   digitalWrite(triger_pin,LOW);
   delayMicroseconds(2);
   digitalWrite(triger_pin,HIGH);
   delayMicroseconds(10);
   digitalWrite(triger_pin,LOW);
-  
   pinMode(echo_pin,INPUT);
   duration = pulseIn(echo_pin,HIGH);
-
-  Serial.print(duration/29/2);
-
+  Serial.print(duration);
   Serial.println();
   delay(100);
+  return duration;
 }
+
